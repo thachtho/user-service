bca71ac1a8ae061e7aa29810ffe990994445ddd7
thachtho <buithanhtho15ig@gmail.com>
2024-08-17 00:08:14 +0700

    ok

--- src/app/controllers/message/rest-api/get-user/get-user.ts
+++ src/app/controllers/message/rest-api/get-user/get-user.ts
@@ -1,12 +1,13 @@
 import { GetByEmailUseCase } from '@application/get-by-email/get-by-email.usecase';
 import { Controller, Get, Param } from '@nestjs/common';
+import { GetUserByEmailDTO } from './get-user.dto';
 
 @Controller('user')
 export class GetUserController {
   constructor(private readonly getByEmailUseCase: GetByEmailUseCase) {}
 
   @Get(':email')
-  findByEmail(@Param('email') email: string) {
-    return this.getByEmailUseCase.excute(email);
+  findByEmail(@Param() params: GetUserByEmailDTO) {
+    return this.getByEmailUseCase.excute(params.email);
   }
 }
--- src/app/lifecycle/app.ts
+++ src/app/lifecycle/app.ts
@@ -1,10 +1,14 @@
 import { kafkaConfig } from 'src/app/infrastructure/common/kafka/kafka.config';
 import { NestFactory } from '@nestjs/core';
+import { useContainer } from 'class-validator';
+import { ValidationPipe } from '@nestjs/common';
 
 export class App {
   static async bootstrap(module: any) {
     const port = process.env.PORT || 5000;
     const app = await NestFactory.create(module);
+    app.useGlobalPipes(new ValidationPipe());
+    useContainer(app.select(module), { fallbackOnErrors: true });
     app.connectMicroservice(kafkaConfig());
     await app.startAllMicroservices();
     await app.listen(port);
