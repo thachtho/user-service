fc87879cefef826bd1c1183f8917cb6d5cce0b92
thachtho <buithanhtho15ig@gmail.com>
2024-08-31 12:14:11 +0700

    remove init database

--- src/app/infrastructure/common/database/database.service.ts
+++ src/app/infrastructure/common/database/database.service.ts
@@ -1,16 +1,11 @@
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { Pool, QueryResult } from 'pg';
-import { CONNECTION_POOL } from './database.module-definition';
 import { Observable, from } from 'rxjs';
-import { createTableQuery } from 'src/asset/sql/create-table';
+import { CONNECTION_POOL } from './database.module-definition';
 
 @Injectable()
-class DatabaseService implements OnModuleInit {
+class DatabaseService {
   constructor(@Inject(CONNECTION_POOL) private readonly pool: Pool) {}
-
-  onModuleInit() {
-    return this.runQuery(createTableQuery);
-  }
 
   runQuery(query: string, params?: unknown[]): Observable<QueryResult<any>> {
     return from(this.pool.query(query, params));
